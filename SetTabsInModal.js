f260ebbd38a6770d4117df8d119aff04b9711c9b
TimPulin <kyliki@yandex.ru>
2021-07-11 13:48:04 +0300

    Create SetTabsInModal.js

---
+++ SetTabsInModal.js
@@ -0,0 +1,27 @@
+function SetTabsInModal(Iam) {
+        let Index,
+            Title_Modal;
+
+        Index = Iam.closest('.tabCalc-links').find('.tabCalc-link').index(Iam);
+        Title_Modal = Iam.val();
+        AddRemove_Active();
+        ResetModal(Iam);
+        ShowHide_tabel ();
+        ShowHeader();
+        Print_Title_Modal();
+
+
+    function AddRemove_Active(){
+        Iam.closest('.tabCalc-links').find('.tabCalc-link.active').removeClass('active');
+        Iam.addClass('active');
+    }
+
+    function ShowHide_tabel(){
+        Iam.closest('.tabCalc-wrap').find('.tabCalc-content.active').removeClass('active');
+        Iam.closest('.tabCalc-wrap').find('.tabCalc-content').eq(Index).addClass('active');
+    }
+
+    function Print_Title_Modal(){
+        Iam.closest('.JS_Section-Modal').find('.headeroutput-title').text(Title_Modal);
+    }
+}
